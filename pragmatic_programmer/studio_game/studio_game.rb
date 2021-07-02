c8e57c1e71f734a9d5c1bba55db6590a3d6d2552
sshussai <sshussai@ualberta.ca>
2021-07-01 22:13:52 -0600

    add functions

--- pragmatic_programmer/studio_game/studio_game.rb
+++ pragmatic_programmer/studio_game/studio_game.rb
@@ -1,4 +1,8 @@
-name1 = "larry"
-health1 = 60
+def say_hello(name, health=100)
+  "I'm #{name} with a health of #{health} as of #{Time.now.strftime("%I:%M:%S")}"
+end
 
-puts "#{name1}'s health is #{health1}"
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
