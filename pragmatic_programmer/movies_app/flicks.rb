e47e17ac4bf3c7d4aab7eddaa0b56345abd627c4
sshussai <sshussai@ualberta.ca>
2021-07-02 09:27:12 -0600

    add class to movies app

--- pragmatic_programmer/movies_app/flicks.rb
+++ pragmatic_programmer/movies_app/flicks.rb
@@ -1,14 +1,21 @@
-def weekday
-  current_time = Time.now
-  current_time.strftime("%A")
+class Movie
+
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank}!"
+  end
+
 end
 
-def movie_listing(title, rank=0)
-  "#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}."
-end
-
-puts movie_listing("goonies", 10)
-puts movie_listing("ghostbusters", 9)
-
-a_title = "goldfinger"
-puts movie_listing(a_title)
