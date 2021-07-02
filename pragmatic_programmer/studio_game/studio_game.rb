b72ae17cc1159ad0b4064be41f79722bd759fd18
sshussai <sshussai@ualberta.ca>
2021-07-02 10:42:48 -0600

    add another class

--- pragmatic_programmer/studio_game/studio_game.rb
+++ pragmatic_programmer/studio_game/studio_game.rb
@@ -32,14 +32,35 @@ class Player
   
 end
 
+class Game
+  
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(p)
+    @players << p
+  end
+
+  def play
+    puts "There are #{@players.count} players in #{@title}"
+    @players.each do |p| 
+      p.blam
+      p.w00t
+      p.w00t
+      puts p
+    end
+  end
+
+end
+
 p1 = Player.new("moe")
 p2 = Player.new("larry", 60)
 p3 = Player.new("curly", 125)
 
-players = [p1, p2, p3]
-players.each do |p|
-  p.blam
-  p.w00t
-  p.w00t
-  puts p
-end
+g = Game.new("Knuckleheads")
+g.add_player(p1)
+g.add_player(p2)
+g.add_player(p3)
+g.play
