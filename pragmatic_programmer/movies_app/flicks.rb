f3623b72187b7e999180566f74a8a689612b4686
sshussai <sshussai@ualberta.ca>
2021-07-18 17:42:53 -0600

    get user input

--- pragmatic_programmer/movies_app/flicks.rb
+++ pragmatic_programmer/movies_app/flicks.rb
@@ -9,4 +9,17 @@ p = Playlist.new("kermit")
 p.add_movie(m1)
 p.add_movie(m2)
 p.add_movie(m3)
-p.play(3)
+
+loop do
+  puts "How many viewings? ('q' to quit)"
+  ans = gets.chomp.downcase
+  case ans
+  when /^\d+$/
+    p.play(ans.to_i)
+  when 'q', 'quit', 'exit'
+    p.print_stats
+    break
+  else
+    puts "Please enter a number or 'q'"
+  end
+end
